1f33f6b72b9bdcbd50283ffd569d4de2e8581fb7
isurumaldeniya <Isuru.Maldeniya@qdos.com.au>
2023-11-29 22:24:12 +1100

    added delete route
    implement more unit tests

--- server/src/database/connect.ts
+++ server/src/database/connect.ts
@@ -5,7 +5,7 @@ dotenv.config()
 
 export default async function connectToMongoDB() {
   try {
-    const db =  mongoose.connect('mongodb://localhost:27017/todo_app');
+    const db = await mongoose.connect('mongodb://localhost:27017/todo_app');
     return db;
   } catch (error) {
     console.error('Error connecting to MongoDB:', error);
